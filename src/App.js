f50597c3c1fdce0c888666f03fa25c5ca6c10a09
CaylinCat <60459726+CaylinCat@users.noreply.github.com>
2024-04-07 14:23:09 -0700

    title page updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import logo from './logo.svg';
 // import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Title from './Title';
+import Title from './Title/Title';
 import Selection from './Selection';
 import SearchPage from './SearchPage';
 import GamePage from './Game';
--- src/Title/Title.js
+++ src/Title/Title.js
@@ -5,12 +5,18 @@ import './Title.css'; // Import CSS file for styles
 const Title = () => {
   return (
     <div className="background">
-      <h1 className="title">Search and Rescue</h1>
-      <h2>Main Menu</h2>
-      <Link className="links" to="/selection">Selection</Link>
+      <div className="logo">Search and Rescue</div>
+      <div className="menu-toggle">&#9776;</div>
+      <div className="title-container">
+        <h1 className="title">Find Your Own Treasure!</h1>
+      </div>
+      <div className="button-container">
+        <a href="/search" className="button search"><img src="search_image.png" alt="Search" />Search</a>
+        <a href="/game" className="button mission"><img src="mission_image.png" alt="Mission" /><b>Mission</b></a>
+        <button onClick={() => window.open('mailto:info@ncmec.org')} className="button contact"><img src="contact_image.png" alt="Contact" />Contact</button>
+      </div>
     </div>
   );
 }
-
 
 export default Title;
